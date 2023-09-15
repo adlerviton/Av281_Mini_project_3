ce1a3a7c1b65021c6596ca8a092c061733b872cf
adlerviton <143657881+adlerviton@users.noreply.github.com>
2023-09-15 11:19:44 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -3,20 +3,14 @@ Test goes here
 
 """
 
-from main import PDdescribe
-import pandas as pd
+from main import describe_with_polars
+import polars as pl
 
 
 def test_desc():
-    """Function calling PDdescribe"""
-    data = {'A': [1, 2, 3, 4, 5], 'B': [10, 20, 30, 40, 50]}
+    """Function calling describe_with_polars"""
     
     # Call the function to be tested
-    result = PDdescribe(data)
+    result = describe_with_polars('nba.csv')
     
-    # Define your expected output
-    expected_output = pd.DataFrame({
-        'A': [5.0, 3.0, 1.581139, 1.0, 2.0, 3.0, 4.0, 5.0],
-        'B': [5.0, 30.0, 15.811388, 10.0, 20.0, 30.0, 40.0, 50.0]
-    }, index=['count', 'mean', 'std', 'min', '25%', '50%', '75%', 'max'])
-    assert result.shape == (8, 2)
+    assert result.shape == (9, 4)
