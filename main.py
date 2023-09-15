e1e31461cfb60259c30afed4186b75315e96a232
adlerviton <143657881+adlerviton@users.noreply.github.com>
2023-09-15 11:15:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-import pandas as pd
+import polars as pl
+import matplotlib.pyplot as plt
 
-def PDdescribe(data):
-    '''function which returns descriptive stats about input data'''
-    df = pd.DataFrame(data)
+def describe_with_polars(filename):
+    """Function which returns descriptive stats about input data using Polars."""
+    df = pl.read_csv(filename)
     return df.describe()
+
+results = describe_with_polars("nba.csv")
+
+print(results)
+
+nba = pl.read_csv("nba.csv")
+plt.figure(figsize=(10, 8))
+plt.scatter(nba["Age"], nba["Weight"])
+plt.title("NBA player Weight vs Age")
+plt.xlabel("Age")
+plt.ylabel("Weight")
+plt.show(block=True)
